26782802fbb1f054cbf0734e82bc04e51b77e790
Catherine Loesch <catherine.loesch@protonmail.com>
2021-06-25 20:34:40 +0100

    update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 let quote = document.getElementById("quote")
 let author = document.getElementById("author")
+
+
 const quoteGeneratorBtn = document.getElementById("quote-generator-btn")
 const comments = document.getElementById("comments")
 const commentSubmitButton = document.getElementById("submit-comment")
-const commentInput = document.getElementById("comment-input")
-
 
 function getQuote(){
          fetch("https://type.fit/api/quotes")
@@ -22,18 +22,28 @@ function getQuote(){
          })
      }
 
+function searchQuotes (searchString){
+         fetch("https://type.fit/api/quotes")
+         .then(res => res.json())
+         .then(data => data["text"])
+         }
+             
+            //  quote.innerHTML = data[index].text
+            //  author.innerHTML = `~  ${data[index].author}`
+            
+
 function addComment(event){
     event.preventDefault();
-    console.log("clicked")
     const commentP = document.createElement("p")
-    commentP.innerHTML = document.getElementById("comment-input").value
+    commentP.innerHTML = `${document.getElementById("name-input").value}: ${document.getElementById("comment-input").value}`
+
     comments.appendChild(commentP)
     document.getElementById("comment-input").value = ""
     }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-     
+    getQuote()
     quoteGeneratorBtn.addEventListener("click", getQuote)
     commentSubmitButton.addEventListener("click", addComment)
     
